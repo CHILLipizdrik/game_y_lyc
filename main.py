a2e0b7359592f47aca3c1166bb23125bb3dd8bb6
chillipizdrik <wasd666player@gmail.com>
2021-02-16 18:43:10 +0300

    Add end screen with sections "play again", "main menu", "exit" all of this work

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Bird:
         self.bird_sp.add(self.bird)
         self.bird_sp.add(self.bird_jump)
 
-    def move(self):
+    def move(self, bird_up, bird_down):
         if jump:
             self.bird.rect.y -= self.jump
             self.bird_jump.rect.y -= self.jump
@@ -54,25 +54,29 @@ class Bird:
             bird_up.rect.y += self.fall
             bird_down.rect.y += self.fall
 
-    def die(self):
-        self.bird.rect.y += 15
-        self.bird_jump.rect.y += 15
-        bird_up.rect.y += 15
-        bird_down.rect.y += 15
-        if self.bird.rect.y >= 775:
-            quit()
+    def die(self, bird_up, bird_down):
+        self.bird.rect.y += 2
+        self.bird_jump.rect.y += 2
+        bird_up.rect.y += 2
+        bird_down.rect.y += 2
+        if self.bird.rect.y >= 780:
+            self.bird_sp.remove(self.bird)
+            self.bird_sp.remove(self.bird_jump)
+            self.bird_d_sp.remove(self.bird)
+            self.bird_j_sp.remove(self.bird_jump)
 
-    def check_collide(self):
+    def check_collide(self, pipe, bird_up, bird_down):
         for pipes_col in pipe.pipes_list_col:
             pipe_up_col, pipe_down_col = pipes_col
             if (bird_up.rect.colliderect(pipe_up_col) or
-                bird_up.rect.colliderect(pipe_down_col) or
-                bird_down.rect.colliderect(pipe_up_col) or
-                bird_down.rect.colliderect(pipe_down_col)):
+                    bird_up.rect.colliderect(pipe_down_col) or
+                    bird_down.rect.colliderect(pipe_up_col) or
+                    bird_down.rect.colliderect(pipe_down_col)):
                 self.dead = True
+                time.sleep(1)
             else:
                 if not self.dead:
-                    self.move()
+                    self.move(bird_up, bird_down)
                     pipe.update_pipe()
 
 
@@ -84,6 +88,7 @@ class Pipe:
         self.pipes_list = []
         self.pipes_list_col = []
         self.pipes_sp = pg.sprite.Group()
+
         self.create_pipes_coords()
 
     def create_pipes_coords(self):
@@ -115,9 +120,9 @@ class Pipe:
         self.pipes_sp.add(self.pipe_down)
         self.pipes_list.append([self.pipe_up, self.pipe_down])
 
-        self.pipe_up_col = PipeUp(x, dl_y)
-        self.pipe_down_col = PipeDown(x, dl_y)
-        self.pipes_list_col.append([self.pipe_up_col, self.pipe_down_col])
+        pipe_up_col = PipeUp(x, dl_y)
+        pipe_down_col = PipeDown(x, dl_y)
+        self.pipes_list_col.append([pipe_up_col, pipe_down_col])
 
     def update_pipe(self):
         for pipes in self.pipes_list:
@@ -132,7 +137,6 @@ class Pipe:
             pipe_up_col.rect.x -= self.pipe_speed
             pipe_down_col.rect.x -= self.pipe_speed
 
-
         for pipe in self.pipes_list:
             pipe_up, pipe_down = pipe
             if pipe_up.rect.x <= -176:
@@ -140,6 +144,7 @@ class Pipe:
                 self.pipes_list_col.pop(0)
                 self.pipes_sp.remove(pipe_up)
                 self.pipes_sp.remove(pipe_down)
+
                 self.add_pipes(self.x, random.randint(300, 600))
             if pipe_up.rect.x == 300:
                 self.get_score()
@@ -191,56 +196,13 @@ class BackGround:
         self.background.add(self.bg)
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pg.image.load(fullname)
-
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-def print_score(score):
-    font = pg.font.Font(None, 86)
-    text = font.render(str(score), True, (252, 200, 96))
-    screen.blit(text, (512, 78))
-
-
-def draw_sprites(score):
-    global jump
-
-    screen.fill((0, 0, 0))
-    bg.background.draw(screen)
-    pipe.pipes_sp.draw(screen)
-    if jump:
-        bird.bird_j_sp.draw(screen)
-        jump = False
-    else:
-        bird.bird_d_sp.draw(screen)
-    print_score(score)
-    pg.display.flip()
-
-
-def terminate():
-    pg.quit()
-    sys.exit()
-
-
 class MainMenu:
     def __init__(self):
         self.bg_main = pg.transform.scale(load_image('background.png'), (width, height))
         self.font_normal = pg.font.Font(None, 100)
         self.font_chosen = pg.font.Font(None, 120)
-        self.color_normal = (252, 200, 96)
-        self.color_chosen = (255, 0, 0)
+        self.color_normal = (250, 200, 100)
+        self.color_chosen = (255, 144, 0)
         self.section_start_game = "START GAME"
         self.section_records = "RECORDS"
         self.section_stats = "STATS"
@@ -302,7 +264,6 @@ class MainMenu:
 
     def choose_section(self):
         while True:
-
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     terminate()
@@ -344,28 +305,28 @@ class MainMenu:
 
                     # section start game
                     if (self.ssg[0] < event.pos[0] < self.ssg[2]) and (self.ssg[1] < event.pos[1] < self.ssg[3]) \
-                        and event.button == 1:
+                            and event.button == 1:
                         return 1
 
                     # section records
                     elif (self.sr[0] < event.pos[0] < self.sr[2]) and (self.sr[1] < event.pos[1] < self.sr[3]) \
-                        and event.button == 1:
+                            and event.button == 1:
                         return 2
 
                     # section stats
                     elif (self.sst[0] < event.pos[0] < self.sst[2]) and (self.sst[1] < event.pos[1] < self.sst[3]) \
-                        and event.button == 1:
+                            and event.button == 1:
                         return 3
 
                     # section shop
                     elif (self.ssh[0] < event.pos[0] < self.ssh[2]) and (self.ssh[1] < event.pos[1] < self.ssh[3]) \
-                        and event.button == 1:
+                            and event.button == 1:
                         return 4
 
                     # section exit
                     elif (self.se[0] < event.pos[0] < self.se[2]) and (self.se[1] < event.pos[1] < self.se[3]) \
-                        and event.button == 1:
-                        terminate()
+                            and event.button == 1:
+                        return 5
 
             pg.display.flip()
             clock.tick(FPS)
@@ -375,26 +336,191 @@ class MainMenu:
             self.sections[i][1] = False
 
 
-def game(running):
+class EndScreen:
+    def __init__(self):
+        self.font = pg.font.Font(None, 80)
+        self.color_normal = (250, 200, 100)
+        self.color_chosen = (255, 144, 0)
+        self.section_play_again = "PLAY AGAIN"
+        self.section_main_menu = "EXIT TO MENU"
+        self.section_exit = "EXIT TO DESKTOP"
+        self.sections = [[self.section_play_again, False], [self.section_main_menu, False], [self.section_exit, False]]
+        self.sections_coords = {
+            self.section_play_again: (335, 240, 695, 310),
+            self.section_main_menu: (302, 326, 730, 396),
+            self.section_exit: (253, 412, 776, 482)}
+
+        self.text_coord = [
+            [345, 250, 340, 56],
+            [312, 336, 408, 56],
+            [263, 422, 513, 56]]
+
+        self.rect_coord = [
+            [335, 240, 360, 70],
+            [302, 326, 428, 70],
+            [253, 412, 523, 70]]
+
+        self.spa = self.sections_coords[self.section_play_again]
+        self.smm = self.sections_coords[self.section_main_menu]
+        self.se = self.sections_coords[self.section_exit]
+
+    def end_screen(self):
+        pg.draw.rect(screen, self.color_chosen, (240, 180, 550, 390), 5)
+        for i in range(3):
+            section = self.sections[i]
+            if section[1]:
+                color = self.color_chosen
+            else:
+                color = self.color_normal
+            text = self.font.render(section[0], True, color)
+
+            screen.blit(text, self.text_coord[i])
+            pg.draw.rect(screen, color, self.rect_coord[i], 3)
+
+    def choose_section(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    terminate()
+                if event.type == pg.MOUSEMOTION:
+                    # section play again
+                    if (self.spa[0] < event.pos[0] < self.spa[2]) and (self.spa[1] < event.pos[1] < self.spa[3]):
+                        self.clear_selection()
+                        self.sections[0][1] = True
+                        self.end_screen()
+
+                    # section main menu
+                    elif (self.smm[0] < event.pos[0] < self.smm[2]) and (self.smm[1] < event.pos[1] < self.smm[3]):
+                        self.clear_selection()
+                        self.sections[1][1] = True
+                        self.end_screen()
+
+                    # section exit
+                    elif (self.se[0] < event.pos[0] < self.se[2]) and (self.se[1] < event.pos[1] < self.se[3]):
+                        self.clear_selection()
+                        self.sections[2][1] = True
+                        self.end_screen()
+
+                    else:
+                        self.clear_selection()
+                        self.end_screen()
+
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    # section play again
+                    if (self.spa[0] < event.pos[0] < self.spa[2]) and (self.spa[1] < event.pos[1] < self.spa[3]):
+                        return 1
+
+                    # section main menu
+                    elif (self.smm[0] < event.pos[0] < self.smm[2]) and (self.smm[1] < event.pos[1] < self.smm[3]):
+                        return 2
+
+                    # section exit
+                    elif (self.se[0] < event.pos[0] < self.se[2]) and (self.se[1] < event.pos[1] < self.se[3]):
+                        return 3
+
+            pg.display.flip()
+            clock.tick(FPS)
+
+    def clear_selection(self):
+        for i in range(3):
+            self.sections[i][1] = False
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pg.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def print_score(score):
+    font = pg.font.Font(None, 86)
+    text = font.render(str(score), True, (252, 200, 96))
+    screen.blit(text, (512, 78))
+
+
+def draw_sprites(score, pipe, bird):
     global jump
+
+    screen.fill((0, 0, 0))
+    bg.background.draw(screen)
+    pipe.pipes_sp.draw(screen)
+    if jump:
+        bird.bird_j_sp.draw(screen)
+        jump = False
+    else:
+        bird.bird_d_sp.draw(screen)
+
+    print_score(score)
+
+    pg.display.flip()
+
+
+def terminate():
+    pg.quit()
+    sys.exit()
+
+
+def game():
+    global jump, running
+    bird = Bird()
+    pipe = Pipe()
+    bird_up = BirdUp()
+    bird_down = BirdDown()
+    draw_sprites(pipe.score, pipe, bird)
+
+    # Start game, waiting player
+    start = False
+    while not start:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                start = True
 
     while running:
         jump = False
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                running = False
-            # Do jump
+                pg.quit()
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 if not bird.dead:
                     jump = True
 
-        bird.check_collide()
+        bird.check_collide(pipe, bird_up, bird_down)
 
         if bird.dead:
-            bird.die()
+            while bird.bird.rect.y <= 780:
+                bird.die(bird_up, bird_down)
+                draw_sprites(pipe.score, pipe, bird)
 
-        draw_sprites(pipe.score)
+            running = False
+            section = end_screen.choose_section()
+            if section == 1:
+                running = True
+                break
+            elif section == 2:
+                running = False
+                break
+
+            elif section == 3:
+                running = False
+                terminate()
+
+        draw_sprites(pipe.score, pipe, bird)
         clock.tick(FPS)
+
+    del bird, pipe, bird_down, bird_up
 
 
 if __name__ == '__main__':
@@ -406,28 +532,27 @@ if __name__ == '__main__':
     clock = pg.time.Clock()
     jump = False
 
-    bird = Bird()
-    pipe = Pipe()
     bg = BackGround()
-    bird_up = BirdUp()
-    bird_down = BirdDown()
     main_menu = MainMenu()
+    end_screen = EndScreen()
 
-    section = main_menu.choose_section()
+    while True:
+        section = main_menu.choose_section()
+        if section == 1:
+            running = True
+            while running:
+                game()
 
-    if section == 1:
-        game(True)
+        elif section == 2:
+            pass
 
-    elif section == 2:
-        game(True)
-        pass
+        elif section == 3:
+            pass
 
-    elif section == 3:
-        game(True)
-        pass
+        elif section == 4:
+            pass
 
-    elif section == 4:
-        game(True)
-        pass
+        elif section == 5:
+            terminate()
 
     pg.quit()
